2f8010f4fca0bb36a69da91e28d3fbd4c8e513ef
arek <agnieszkaarek@gmail.com>
2020-11-30 22:16:26 +0000

    Clockport decode on INTSIG4

--- main_top.v
+++ main_top.v
@@ -1,7 +1,7 @@
 
 `timescale 1ns / 1ps
 /*
-    Copyright (C) 2020, Stephen J. Leary
+    Copyright (C) 2020, Stephen J. Leary & Arek Makarenko
     All rights reserved.
     
     This file is part of CD32 USB Riser
@@ -20,7 +20,8 @@
 
 
 module main_top(
-
+	
+	//CPU
     input CLKCPU_A, 
     input AS20, 
     input DS20, 
@@ -35,43 +36,46 @@ module main_top(
     output PUNT_OUT,
 	 
 	 //
-	 output INTSIG1,
-	 output INTSIG2,
-	 output INTSIG3,
-	 output INTSIG4,
-	 output INTSIG5,
-	 input INTSIG6,
-	 input INTSIG7,
-	 output INTSIG8, //SPI_NSS
+	 output INTSIG1, //RTC Interrupt line
+	 output INTSIG2, //Buttons Interrupt
+	 output INTSIG3, //  A3 line
+	 output INTSIG4, //Clockport interrupt
+	 output INTSIG5, //A5 line
+	 input INTSIG6, //Enable Mouse and Joystick port override
+	 input INTSIG7, // STM32 ready - cpu release signal
+	 output INTSIG8, // JOY registeres interrupt and Direct Address
 
+    // SPI COMMS Not used - might be reused
 
-
-    // SPI COMMS 
-
-    input SPI_CK, 
-    input SPI_MOSI, 
-    output SPI_MISO
+    input SPI_CK,   // not used 
+    input SPI_MOSI, //not used
+    output SPI_MISO //not used
 
 );
 
-wire rtc_decode = A[23:8] == 16'b1101_1100_0000_0000; //RTC registers at $DC0000 - $DCFFFF read,
-wire JOYDATA = A[23:3] == {20'hDFF00, 1'b1}; 
+wire rtc_decode = A[23:8] == 16'b1101_1100_0000_0000; //RTC registers at $DC0000 - $DC00FF,
+wire clockport = A[23:16] ==  16'b1101_1000;  //D80000 to $D8FFFF - clockport addresses
+wire JOYDATA = A[23:3] == {20'hDFF00, 1'b1};
+wire JOYTEST = A[23:1] == {23'hDFF03, 3'b011};  
 
 wire POTGOR_decode = A[23:1] == {20'hDFF01, 3'b011}; // POTGOR DFF016 
 wire POTGO_decode = A[23:1] == {20'hDFF03, 3'b010};  // POTGO DFF034
 
-wire CIAAPRA_decode = A[23:1] == {20'hBFE00,3'b000}; // CIAAPRA BFE001    
+wire CIAAPRA_decode = A[23:8] == {20'hBFE0}; // CIAAPRA BFE001  
+wire CIAADRA_decode = A[23:0] == {24'hBFE201}; // CIAADDRA BFE201
 
-//wire POTGOR_decode = A[23:3] == {20'hDFF01, 1'b0}; // POTGOR DFF016 //DFF012 DFF014
+wire Direct_Access = A[23:1] == {23'hBA000, 3'b011}; // Direct access to set up sensitivity
 
-wire enable = INTSIG6 == 1'b1;
+wire enable = INTSIG6 == 1'b1;		//Enable ports override
 
 
-wire punt_int = rtc_decode |( (JOYDATA|POTGOR_decode|POTGO_decode|CIAAPRA_decode)&enable );
+wire punt_int = clockport | rtc_decode | Direct_Access |( (JOYDATA|JOYTEST|POTGOR_decode|POTGO_decode|CIAAPRA_decode|CIAADRA_decode)&enable );
 
 reg rtc_int;
+reg da_int;
 reg joy_int;
 reg button_int;
+reg clockport_int;
 
 reg[1:0] intsig_int;
 reg punt_ok;
@@ -85,26 +89,30 @@ always @(posedge CLKCPU_A) begin
 
 	punt_ok <= PUNT_IN & punt_int;
 		
-	
+// Wait for Address Strobe to rise interrupt.
 	if (AS20 == 1'b0) begin
+		da_int <= PUNT_IN & Direct_Access;
 		rtc_int <= PUNT_IN & rtc_decode;
-		joy_int <= PUNT_IN & JOYDATA;
-		button_int <= PUNT_IN & (POTGOR_decode|POTGO_decode|CIAAPRA_decode);
+		joy_int <= PUNT_IN & (JOYDATA|CIAADRA_decode);
+		button_int <= PUNT_IN & (POTGOR_decode|POTGO_decode|CIAAPRA_decode|JOYTEST);
+		clockport_int <= PUNT_IN & clockport;
 	end else begin 
+		da_int <= 1'b0;
 		rtc_int <= 1'b0;
 		joy_int <= 1'b0;
 		button_int <= 1'b0;
-		end
+		clockport_int <= 1'b0;
+	end
 	
 	
-	
+	//Release CPU only on INTSIG edge change.
 	actual_acknowledge <= ack == 2'b01;  
    ack <= {ack[0], INTSIG7};
   
 end
 
 
-
+// Insert waitstates for punt_int addresses and release it on raising edge of INTSIG7
 always @(posedge CLKCPU_A or posedge AS20) begin 
 	if (AS20 == 1'b1) begin 
 		intsig_int <= 2'b11;
@@ -123,14 +131,18 @@ end
 // punt works by respecting the accelerator punt over our punt.
 assign PUNT_OUT = PUNT_IN ? ( punt_int ? 1'b0 : 1'bz) : 1'b0;
 
-assign INTSIG2 = button_int&enable;
+
+//STM32 Interrupts.
 assign INTSIG1 = rtc_int;
-assign INTSIG8 = joy_int&enable;   
+assign INTSIG2 = button_int&enable;
+assign INTSIG8 = da_int | (joy_int&enable);  
+assign INTSIG4 = clockport_int; 
 
 
+//Waitstates
 assign DSACK = punt_ok?intsig_int:2'bzz ;
  
-
+//Missing Address lines on STM32
 assign INTSIG3 = A[3];
 assign INTSIG5 = A[5];
 
